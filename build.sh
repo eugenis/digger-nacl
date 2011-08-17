253c88f8d7d01e5cc6b32408cb362649ed0f198f
Evgeniy Stepanov <eugenis@google.com>
2011-08-17 15:31:02 +0400

    Optimize, strip, and update manifest.

--- build.sh
+++ build.sh
@@ -4,8 +4,11 @@ set -e
 
 rm -f app/digger-x86_32.nexe app/digger-x86_64.nexe
 
-make -f Makefile.sdl clean
-NACL_BITS=   make -f Makefile.sdl && mv digger app/digger_x86-32.nexe
+STRIP32=${NACL_TOOLCHAIN_ROOT}/bin/nacl-strip
+STRIP64=${NACL_TOOLCHAIN_ROOT}/bin/nacl64-strip
 
 make -f Makefile.sdl clean
-NACL_BITS=64 make -f Makefile.sdl && mv digger app/digger_x86-64.nexe
+NACL_BITS=   make -f Makefile.sdl && ${STRIP32} digger && mv digger app/digger_x86-32.nexe
+
+make -f Makefile.sdl clean
+NACL_BITS=64 make -f Makefile.sdl && ${STRIP64} digger && mv digger app/digger_x86-64.nexe
